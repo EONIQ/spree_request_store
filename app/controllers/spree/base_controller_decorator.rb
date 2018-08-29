5990a89872ac7c0734922bfe8b2af03e7f7416e7
charleswong28 <chung.triniti@gmail.com>
2018-08-29 17:09:10 +0800

    Fix set current info

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -3,7 +3,7 @@ require 'request_store'
 Spree::BaseController.class_eval do
   before_action :set_current_info
 
-  def set_current_currency
+  def set_current_info
     RequestStore.store[:current_user] = spree_current_user || current_spree_user
     RequestStore.store[:current_currency] = current_currency
     RequestStore.store[:current_store] = current_store
