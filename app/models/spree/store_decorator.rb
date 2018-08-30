893ff468da0b2dc689e0e54c37c233d68d6675d9
charleswong28 <chung.triniti@gmail.com>
2018-08-31 02:00:25 +0800

    Add with_store, with_user, with_currency function

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -12,4 +12,19 @@ Spree::Store.class_eval do
   def self.current_store
     RequestStore.store[:current_store] || Spree::Store.current
   end
+
+  def self.with_store(store)
+    RequestStore.store[:current_store] = store
+    yield
+  end
+
+  def self.with_currency(currency)
+    RequestStore.store[:current_currency] = currency
+    yield
+  end
+
+  def self.with_user(user)
+    RequestStore.store[:current_user] = user
+    yield
+  end
 end
